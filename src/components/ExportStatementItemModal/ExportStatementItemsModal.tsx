c6e767300c1362a439ed66955af4c31a06125ab6
Grem <giuliano.gremlich@supsi.ch>
2025-07-21 11:14:29 +0200

    style 🌈:  Add name of statement item and rule to CSV

--- src/components/ExportStatementItemModal/ExportStatementItemsModal.tsx
+++ src/components/ExportStatementItemModal/ExportStatementItemsModal.tsx
@@ -18,22 +18,28 @@ type Props = {
 
 const ExportStatementItemsModal = (props:Props)=> {
 
-    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+    const [selectedFromDate, setSelectedFromDate] = useState<Date | null>(null);
+    const [selectedToDate, setSelectedTpDate] = useState<Date | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const handleDateFromChange = (date: Date | null) => {
-        setSelectedDate(date);
+        setSelectedFromDate(date);
     };
 
+    const handleDateToChange = (date: Date | null) => {
+        setSelectedTpDate(date);
+    };
 
     const exportAllStatementItemsToCsv = async () => {
+        setIsLoading(true);
         try {
             if (props.statementItemByCategoryMap.size === 0) {
                 console.warn('No statement items available to export.');
                 return;
             }
 
-            if (selectedDate === null) {
-                console.warn('No date selected.');
+            if (selectedFromDate === null || selectedToDate === null) {
+                console.warn('Select a date range before exporting.');
                 return;
             }
 
@@ -42,57 +48,81 @@ const ExportStatementItemsModal = (props:Props)=> {
                 console.warn('No statement items found for export.');
                 return;
             }
+
+            const normalizedFromDate = new Date(selectedFromDate);
+            normalizedFromDate.setUTCHours(0, 0, 0, 0); // Normalize to start of day
+            const normalizedToDate = new Date(selectedToDate);
+            normalizedToDate.setUTCHours(23, 59, 59, 999); // Normalize to end of day
             const csvData = [];
             for(const statementItem of allStatementItems) {
-                const recordsAndTransactions = await statementItemsClient.getStatementItemRecordsWithTransactions(statementItem.id,new Date(Date.UTC(
-                    selectedDate.getUTCFullYear(),
-                    selectedDate.getMonth(),
-                    selectedDate.getDay()
-                )));
-
-                csvData.push(convertTransactionsToCSV(recordsAndTransactions));
+                for(let date = new Date(normalizedFromDate); date <= normalizedToDate; date.setDate(date.getDate() + 1)) {
+                    const recordsAndTransactions = await statementItemsClient.getStatementItemRecordsWithTransactions(statementItem.id,date);
+                    csvData.push(await convertTransactionsToCSV(recordsAndTransactions));
+                }
             }
             const csvDataWithoutUndefined:string[] = csvData.filter((data): data is string => data !== undefined && true &&  data !== '');
             console.log(csvDataWithoutUndefined);
+
             downloadCSVFromArray(csvDataWithoutUndefined);
         } catch (error) {
             console.error('Error exporting CSV:', error);
+        }finally {
+            setIsLoading(false);
+            props.onClose();
         }
     }
 
     return (
         <div className={`modal  ${props.isOpen ? 'modal-open' : ''}`}>
-            <div className="modal-box h-auto min-h-[500px] overflow-y-auto ">
+            <div className="flex flex-col modal-box h-auto min-h-[500px] overflow-y-auto">
                 <h3 className="font-bold text-lg mb-4">{props.title}</h3>
-                <div className="form-control w-full">
-                    <label className="label">
-                        <span className="label-text">Select Date:</span>
-                    </label>
-                    <ReactDatePicker
-                        selected={selectedDate}
-                        onChange={handleDateFromChange}
-                        className="input input-bordered"
-                        dateFormat="yyyy-MM-dd"
-                    />
-                </div>
-
-                <div className="modal-action">
-                    <button
-                        className="btn btn-ghost"
-                        onClick={props.onClose}
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        className={`btn btn-primary`}
+                <div className="flex flex-col grow justify-between">
+                {isLoading && <div className="justify-center loading loading-spinner loading-lg"></div>}
+                {!isLoading && <>
+                    <div className="flex form-control w-full justify-center items-center">
+                        <div>
+                        <label className="label">
+                            <span className="label-text">Select From Date:</span>
+                        </label>
+                        <ReactDatePicker
+                            selected={selectedFromDate}
+                            onChange={handleDateFromChange}
+                            className="input input-bordered"
+                            dateFormat="yyyy-MM-dd"
+                        />
+                        </div>
+                        <div>
+                            <label className="label">
+                                <span className="label-text">Select To Date:</span>
+                            </label>
+                            <ReactDatePicker
+                                selected={selectedToDate}
+                                onChange={handleDateToChange}
+                                className="input input-bordered"
+                                dateFormat="yyyy-MM-dd"
+                            />
+                        </div>
+                    </div>
+                    <div className="modal-action justify-end">
+                        <button
+                            className="btn btn-ghost"
+                            onClick={props.onClose}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            className={`btn btn-primary`}
                         onClick={exportAllStatementItemsToCsv}
-                        disabled={!selectedDate}
+                        disabled={!selectedFromDate || !selectedToDate}
                     >
-                    <>
-                        <Download className="h-4 w-4 mr-2"/>
-                        Export CSV
-                    </>
+                        <>
+                            <Download className="h-4 w-4 mr-2"/>
+                            Export CSV
+                        </>
                     </button>
+                </div>
+                </>
+                }
                 </div>
             </div>
         </div>
--- src/utility/FileManagement.ts
+++ src/utility/FileManagement.ts
@@ -1,4 +1,5 @@
 import { AccountingTransaction, AccountingTransactionWithTotals, DailyTransactionRecord } from "@derp/company-canister";
+import {dispatchRulesClient, statementItemsClient} from "../api/icp";
 
 
 export const downloadCSV = (csvContent: string, filename = 'transactions.csv'): void => {
@@ -49,14 +50,15 @@ interface TransactionData {
 // Tipo per una riga CSV
 interface TransactionCSVRow {
     record_id: number;
-    statement_item_id: number;
+    statement_item: string;
     record_date: string;
     issue_date: string;
     value_date: string;
     total: number;
     dlterp_id: string;
     txType: string;
-    dispatch_rule_id: string;
+    dispatch_rule_type: string;
+    dispatch_rule_operation: string;
     currency: string;
     source: string;
     storeId: number;
@@ -71,21 +73,26 @@ interface TransactionCSVRow {
 // Builder per costruire una riga CSV da TransactionData
 class TransactionCSVRowBuilder {
 
-    static build(data: TransactionData): TransactionCSVRow {
+    static async build(data: TransactionData): Promise<TransactionCSVRow> {
         const { record, transaction } = data;
         const header = transaction.Header;
-
+        const statementItem = await statementItemsClient.getStatementItem(record.parentStatementItemId);
+        let dispatchRule = null;
+        if(record.originalRuleId !== undefined || true) {
+            dispatchRule = await dispatchRulesClient.getDispatchRule(Number(record.originalRuleId));
+        }
         const row: TransactionCSVRow = {
             // Campi da DailyTransactionRecord
             record_id: record.id,
-            statement_item_id: record.parentStatementItemId,
+            statement_item: statementItem.name,
             record_date: this.formatDate(record.date),
             issue_date: this.formatOptionalDate(header.IssueDate),
             value_date: this.formatOptionalDate(header.ValueDate),
             total: record.total,
             dlterp_id: record.transactionId,
             txType: record.txType,
-            dispatch_rule_id: this.formatOptionalNumber(record.originalRuleId),
+            dispatch_rule_type: dispatchRule.ruleType,
+            dispatch_rule_operation: dispatchRule.accountingOperation,
             currency: this.formatOptionalString(header.Currency),
             source: this.formatOptionalString(header.Source),
             storeId: header.StoreId,
@@ -156,14 +163,15 @@ export const escapeCsvValue = (value: any): string => {
 // Definizione delle colonne del CSV nell'ordine desiderato
 export const CSV_COLUMNS: (keyof TransactionCSVRow)[] = [
     "record_id",
-    "statement_item_id",
+    "statement_item",
     "record_date",
     "issue_date",
     "value_date",
     "total",
     "dlterp_id",
     "txType",
-    "dispatch_rule_id",
+    "dispatch_rule_type",
+    "dispatch_rule_operation",
     "currency",
     "source",
     "storeId",
@@ -175,13 +183,13 @@ export const CSV_COLUMNS: (keyof TransactionCSVRow)[] = [
     "total_incl_tax"
 ];
 
-export const convertTransactionsToCSV = (data: TransactionData[]): string => {
+export const convertTransactionsToCSV = async (data: TransactionData[]): Promise<string> => {
     if (data.length === 0) {
         return '';
     }
 
     // Costruisci le righe CSV usando il builder
-    const csvRows = data.map(item => TransactionCSVRowBuilder.build(item));
+    const csvRows = await Promise.all(data.map(item => TransactionCSVRowBuilder.build(item)));
 
     // Crea l'header del CSV
     // const csvHeader = CSV_COLUMNS.map(column => escapeCsvValue(column)).join(',');
